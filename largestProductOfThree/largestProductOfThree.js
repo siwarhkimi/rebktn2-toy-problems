880540d671c70fc5d90f3ce87c6760e0db0e1e41
siwarhkimi <cywarhkimi@gmail.com>
2020-03-24 09:50:58 +0100

    toy problem: largest Product Of three

--- largestProductOfThree/largestProductOfThree.js
+++ largestProductOfThree/largestProductOfThree.js
@@ -7,4 +7,17 @@ largestProductOfThree([ 0, 2, 3 ]) //	0
 largestProductOfThree([1, 4, -5, -2]) // 40
 */
 
-function largestProductOfThree(array) {}
+function largestProductOfThree(array) {
+    if(array.length >= 3) {
+        for(var i = 0; i < array.length; i++) {
+            if(array[i] < 0) {
+                array[i] = - array[i];
+            }
+        }
+        array.sort();
+    
+        return array[array.length - 3] * array[array.length - 2] * array[array.length - 1];
+    } else {
+        return "Please enter an array with length at least 3";
+    }
+}
