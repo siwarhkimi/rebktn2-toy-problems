fd61bf09813b8fe9974cd577d1aceaec8b45a2d7
siwarhkimi <cywarhkimi@gmail.com>
2020-03-25 11:51:54 +0100

    almost there

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -1,15 +1,34 @@
 /*
- * Given a sorted array, find the index of an element
- * using a binary search algorithm.
- *
- * Example usage:
- *
- * var index = binarySearch([1, 2, 3, 4, 5], 4);
- * console.log(index); // 3
- * var index = binarySearch([1, 2, 3, 4, 5], 8);
- * console.log(index); // null
- */
+* Given a sorted array, find the index of an element
+* using a binary search algorithm.
+*
+* Example usage:
+*
+* var index = binarySearch([1, 2, 3, 4, 5], 4);
+* console.log(index); // 3
+* var index = binarySearch([1, 2, 3, 4, 5], 8);
+* console.log(index); // null
+*/
+
 
 var binarySearch = function (array, target) {
+    var start = 0;
+    var end = array.length;
+    var middle = Math.floor((array[start]+array[end]) / 2);   
+    
+    while(start < end) {
+        if(array[middle]===target) {
+            return middle;
+        }
+        else if(array[middle] < target) {
+            end = middle - 1;
+            middle =  Math.floor((array[start]+array[end]) / 2);
+        } else if(array[middle] > target) {
+            start = middle + 1;
+            middle =  Math.floor((array[start]+array[end]) / 2);
+        }
+    }
+    return null;
+    
 };
 
